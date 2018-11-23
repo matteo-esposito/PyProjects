bf3f380f49006f8260a6920f8b3eb28cae0c19b8
Matteo <ds.mesposito@gmail.com>
2018-11-23 13:44:03 -0500

    Create integer-to-binary.py

---
+++ integer-to-binary.py
@@ -0,0 +1,42 @@
+import math
+
+def intToBin(n):
+    '''
+    Convert a postive integer to its binary representation.
+    MILA sample interview question
+    Fall 2018
+    '''
+    binaryVal = []
+    intVal = n
+    
+    # Handle base cases then general case
+    if n < 0:
+        return(str(intVal) + " has no binary represention.")
+    elif n == 0: 
+        binaryVal.append('0')
+    elif n == 1: 
+        binaryVal.append('1')
+    else:
+        exp = math.floor(math.log(n) / math.log(2))
+        
+        while (exp >= 0):
+            
+            binFactor = 2 ** exp
+            
+            if binFactor <= n:
+                binaryVal.append('1')
+                n -= binFactor
+            else:
+                binaryVal.append('0')
+                
+            exp -= 1 # update exponent
+                
+    return(str(intVal) + " in binary is: " + "".join(binaryVal))
+        
+    
+# Sample set
+nums = [-34,-2,0,1,33,70,10042344232]
+
+for n in nums:
+    print(intToBin(n)) 
+
